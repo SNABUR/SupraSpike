7857dd0790ebae07c22636d579331f4fc0968305
SNABUR <promisesimetry@gmail.com>
2024-12-19 11:25:33 -0500

    done

--- app/page.tsx
+++ app/page.tsx
@@ -165,9 +165,7 @@ export default function Home() {
       <div className="md:flex items-center gap-6">
         {/* Botón para MemeFactory */}
         <Link
-          //href="/launchpad"
-          href="/"
-
+          href="/launchpad"
           className="hidden md:flex bg-gradient-to-r from-yellow-500 to-orange-700 text-white font-bold py-2 px-4 rounded-full shadow-lg hover:from-yellow-300 hover:to-orange-400 hover:shadow-xl transition w-full md:w-auto text-center"
         >
           LaunchPad 🎨
